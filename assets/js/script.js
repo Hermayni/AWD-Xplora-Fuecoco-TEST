d09a6d63e20a8ff2ce2df6a82c6f398404cc8b1c
Hermione <151361941+Hermayni@users.noreply.github.com>
2025-03-30 16:29:40 +0800

    fix(home): js linking

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,11 @@ function scrollToSection(sectionId) {
 document.addEventListener("DOMContentLoaded", function () {
     // Redirect to login page when clicking the profile icon
     document.querySelector(".profile-icon").addEventListener("click", function () {
-        window.location.href = "/pages/page2/index.html"; // Adjust the path as needed
+        window.location.href = /pages/page2/index.html ; // Adjust the path as needed
     });
 
     // Redirect to login page when clicking the "Click to View Top Spots" button
     document.querySelector(".hero-text .btn").addEventListener("click", function () {
-        window.location.href = "/pages/page2/index.html"; // Adjust the path as needed
+        window.location.href = /pages/page2/index.html ; // Adjust the path as needed
     });
 });
